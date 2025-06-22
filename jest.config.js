25bc4b96c65dccf70b7a5a344d5c3f0c65218823
polofac812 <44112753-polofac812@users.noreply.replit.com>
2025-06-22 21:24:32 +0000

    updates

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/client/src/$1',
+    '^@shared/(.*)$': '<rootDir>/shared/$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  collectCoverageFrom: [
+    'server/**/*.{ts,tsx}',
+    'client/src/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/dist/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.{ts,tsx}',
+    '<rootDir>/**/*.(test|spec).{ts,tsx}'
+  ],
+  testTimeout: 30000,
+  maxWorkers: 1,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  forceExit: true,
+  detectOpenHandles: true
+};

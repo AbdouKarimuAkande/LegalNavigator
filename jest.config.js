597af7dc033f84b1532fff457a59afe49b7ab96e
bahoxo4720 <44150182-bahoxo4720@users.noreply.replit.com>
2025-06-23 03:25:42 +0000

    update

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,21 @@
 
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/server', '<rootDir>/client/src'],
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/__tests__/**/*.test.+(ts|tsx|js)',
     '**/*.(test|spec).+(ts|tsx|js)'
   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    'test-utils.ts',
+    'setup.ts',
+    '.*\\.disabled$'
+  ],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      useESM: true
-    }],
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   collectCoverageFrom: [
     'server/**/*.{ts,tsx}',
@@ -21,20 +25,15 @@ export default {
     '!server/index.ts',
     '!**/node_modules/**',
     '!**/dist/**',
+    '!**/*test-utils*',
+    '!**/*setup*'
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: [
-    'text',
-    'lcov',
-    'html',
-    'json-summary'
-  ],
-  testTimeout: 30000,
+  testTimeout: 10000,
+  maxWorkers: 1,
+  forceExit: true,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/client/src/$1',
     '^@shared/(.*)$': '<rootDir>/shared/$1'
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(wouter|node-fetch|nanoid)/)'
-  ]
+  }
 };
